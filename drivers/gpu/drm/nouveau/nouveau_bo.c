4c5391ac2125188d3e571082686e6873b8ce00da
Matthew Auld <matthew.auld@intel.com>
2021-09-29 14:26:27 +0100

    BACKPORT: drm/ttm: s/FLAG_SG/FLAG_EXTERNAL/
    It covers more than just ttm_bo_type_sg usage, like with say dma-buf,
    since one other user is userptr in amdgpu, and in the future we might
    have some more. Hence EXTERNAL is likely a more suitable name.
    
    v2(Christian):
      - Rename these to TTM_TT_FLAGS_*
      - Fix up all the holes in the flag values
    
    Suggested-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Matthew Auld <matthew.auld@intel.com>
    Cc: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Cc: Christian König <christian.koenig@amd.com>
    Acked-by: Christian König <christian.koenig@amd.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210929132629.353541-1-matthew.auld@intel.com
    Signed-off-by: Christian König <christian.koenig@amd.com>
    (cherry picked from commit 43d46f0b78bba5dc5ffb6f1b9a1d4c8d0c5dd1fc)
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>
    Signed-off-by: Hsuan-Yu Lin <shane.lin@canonical.com>

--- drivers/gpu/drm/nouveau/nouveau_bo.c
+++ drivers/gpu/drm/nouveau/nouveau_bo.c
@@ -1263,7 +1263,7 @@ nouveau_ttm_tt_populate(struct ttm_device *bdev,
 	struct ttm_tt *ttm_dma = (void *)ttm;
 	struct nouveau_drm *drm;
 	struct device *dev;
-	bool slave = !!(ttm->page_flags & TTM_PAGE_FLAG_SG);
+	bool slave = !!(ttm->page_flags & TTM_TT_FLAG_EXTERNAL);
 
 	if (ttm_tt_is_populated(ttm))
 		return 0;
@@ -1286,7 +1286,7 @@ nouveau_ttm_tt_unpopulate(struct ttm_device *bdev,
 {
 	struct nouveau_drm *drm;
 	struct device *dev;
-	bool slave = !!(ttm->page_flags & TTM_PAGE_FLAG_SG);
+	bool slave = !!(ttm->page_flags & TTM_TT_FLAG_EXTERNAL);
 
 	if (slave)
 		return;
