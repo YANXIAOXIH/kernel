bd2a19b143f96b9ba55d3e9e1541fef2e4a6731a
Sean Wang <sean.wang@mediatek.com>
2021-12-02 02:02:46 +0800

    UPSTREAM: Bluetooth: btmtksdio: handle runtime pm only when sdio_func is available
    Runtime pm ops is not aware the sdio_func status that is probably
    being disabled by btmtksdio_close. Thus, we are only able to access the
    sdio_func for the runtime pm operations only when the sdio_func is
    available.
    
    Fixes: 7f3c563c575e7 ("Bluetooth: btmtksdio: Add runtime PM support to SDIO based Bluetooth")
    Co-developed-by: Mark-yw Chen <mark-yw.chen@mediatek.com>
    Signed-off-by: Mark-yw Chen <mark-yw.chen@mediatek.com>
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    (cherry picked from commit 4b4b2228f521c338030b1f310a5dee73fd7d8f26
     https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git master)
    
    BUG=b:178754244
    TEST=Build on corsola
    
    Signed-off-by: Sean Wang <objelf@gmail.com>
    Change-Id: Ib47fd6fe567abce028a36469c3382a703101cb1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3554498
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Abhishek Pandit-Subedi <abhishekpandit@google.com>
    Tested-by: Abhishek Pandit-Subedi <abhishekpandit@google.com>
    Commit-Queue: Abhishek Pandit-Subedi <abhishekpandit@google.com>
    Signed-off-by: Hsuan-Yu Lin <shane.lin@canonical.com>

--- drivers/bluetooth/btmtksdio.c
+++ drivers/bluetooth/btmtksdio.c
@@ -100,6 +100,7 @@ MODULE_DEVICE_TABLE(sdio, btmtksdio_table);
 
 #define BTMTKSDIO_TX_WAIT_VND_EVT	1
 #define BTMTKSDIO_HW_TX_READY		2
+#define BTMTKSDIO_FUNC_ENABLED		3
 
 struct mtkbtsdio_hdr {
 	__le16	len;
@@ -541,6 +542,8 @@ static int btmtksdio_open(struct hci_dev *hdev)
 	if (err < 0)
 		goto err_release_host;
 
+	set_bit(BTMTKSDIO_FUNC_ENABLED, &bdev->tx_state);
+
 	/* Get ownership from the device */
 	sdio_writel(bdev->func, C_FW_OWN_REQ_CLR, MTK_REG_CHLPCR, &err);
 	if (err < 0)
@@ -642,6 +645,7 @@ static int btmtksdio_close(struct hci_dev *hdev)
 	if (err < 0)
 		bt_dev_err(bdev->hdev, "Cannot return ownership to device");
 
+	clear_bit(BTMTKSDIO_FUNC_ENABLED, &bdev->tx_state);
 	sdio_disable_func(bdev->func);
 
 	sdio_release_host(bdev->func);
@@ -1061,6 +1065,9 @@ static int btmtksdio_runtime_suspend(struct device *dev)
 	if (!bdev)
 		return 0;
 
+	if (!test_bit(BTMTKSDIO_FUNC_ENABLED, &bdev->tx_state))
+		return 0;
+
 	sdio_claim_host(bdev->func);
 
 	sdio_writel(bdev->func, C_FW_OWN_REQ_SET, MTK_REG_CHLPCR, &err);
@@ -1086,6 +1093,9 @@ static int btmtksdio_runtime_resume(struct device *dev)
 
 	bdev = sdio_get_drvdata(func);
 	if (!bdev)
+		return 0;
+
+	if (!test_bit(BTMTKSDIO_FUNC_ENABLED, &bdev->tx_state))
 		return 0;
 
 	sdio_claim_host(bdev->func);
