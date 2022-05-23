54b7ec12a4fd13c848311b29b8cb85ec13ba90e3
Rex-BC Chen <rex-bc.chen@mediatek.com>
2022-05-23 17:33:32 +0800

    UPSTREAM: clk: mediatek: reset: Merge and revise reset register function
    There are two versions for clock reset register control for MediaTek
    SoCs. The old hardware is one bit per reset control, and does not
    have separate registers for bit set, clear and read-back operations.
    This matches the scheme supported by the simple reset driver.
    
    However, because we need to use different data structure from
    reset_simple_data, we can not use the operation of simple reset
    driver.
    For this reason, we keep the original functions and name this version
    as "MTK_RST_SIMPLE".
    
    In this patch:
    - Add a version enumeration to separate different reset hardware.
    - Merge the reset register function of simple and set_clr into one
      function "mtk_register_reset_controller".
    - Rename input variable "num_regs" to "rst_bank_nr" to avoid
      confusion. This variable is used to define the quantity of reset bank.
    - Document mtk_reset_version and mtk_register_reset_controller.
    
    Change-Id: Ida13e0b6c4e0b7471bb7b169de4b1e6db26440d3
    Signed-off-by: Rex-BC Chen <rex-bc.chen@mediatek.com>
    Reviewed-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Reviewed-by: Nícolas F. R. A. Prado <nfraprado@collabora.com>
    Tested-by: Nícolas F. R. A. Prado <nfraprado@collabora.com>
    Link: https://lore.kernel.org/r/20220523093346.28493-6-rex-bc.chen@mediatek.com
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>
    cherry picked from commit 370bf62869695003c2994d3d98769ccde6b26083
    Signed-off-by: Chris-QJ Chen <chris-qj.chen@mediatek.com>
    Signed-off-by: Hsuan-Yu Lin <shane.lin@canonical.com>

--- drivers/clk/mediatek/clk-mt2701.c
+++ drivers/clk/mediatek/clk-mt2701.c
@@ -790,7 +790,7 @@ static int mtk_infrasys_init(struct platform_device *pdev)
 	if (r)
 		return r;
 
-	mtk_register_reset_controller(node, 2, 0x30);
+	mtk_register_reset_controller(node, 2, 0x30, MTK_RST_SIMPLE);
 
 	return 0;
 }
@@ -915,7 +915,7 @@ static int mtk_pericfg_init(struct platform_device *pdev)
 	if (r)
 		return r;
 
-	mtk_register_reset_controller(node, 2, 0x0);
+	mtk_register_reset_controller(node, 2, 0x0, MTK_RST_SIMPLE);
 
 	return 0;
 }
