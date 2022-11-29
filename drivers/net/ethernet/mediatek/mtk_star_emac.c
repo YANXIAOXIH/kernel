0edf6f1a389e55bb03fa1af5167e6ed520d337fb
Howard Lin <howard-yh.lin@mediatek.com>
2022-11-29 11:17:20 +0800

    GENIO: net: ethernet: mediatek: fix uninitialized value
    Fix coverity reported variable used without being initialized.
    
    drivers/net/ethernet/mediatek/mtk_star_emac.c
    
    static inline void
    mtk_star_enable_dma_irq(struct mtk_star_priv *priv, bool rx, bool tx)
    {
    (1) Event var_decl:	Declaring variable "value" without initializer.
    Also see events:	[uninit_use]
    	u32 value;
    
    static inline void
    mtk_star_disable_dma_irq(struct mtk_star_priv *priv, bool rx, bool tx)
    {
    (1) Event var_decl:	Declaring variable "value" without initializer.
    Also see events:	[uninit_use]
    	u32 value;
    
    Change-Id: I2516a6a7b91acc32c2ab0189e7dcfb165051281e
    Signed-off-by: Howard Lin <howard-yh.lin@mediatek.com>
    Signed-off-by: Hsuan-Yu Lin <shane.lin@canonical.com>

--- drivers/net/ethernet/mediatek/mtk_star_emac.c
+++ drivers/net/ethernet/mediatek/mtk_star_emac.c
@@ -445,7 +445,7 @@ static inline void mtk_star_nic_disable_pd(struct mtk_star_priv *priv)
 static inline void
 mtk_star_enable_dma_irq(struct mtk_star_priv *priv, bool rx, bool tx)
 {
-	u32 value;
+	u32 value = 0;
 
 	regmap_read(priv->regs, MTK_STAR_REG_INT_MASK, &value);
 
@@ -460,7 +460,7 @@ mtk_star_enable_dma_irq(struct mtk_star_priv *priv, bool rx, bool tx)
 static inline void
 mtk_star_disable_dma_irq(struct mtk_star_priv *priv, bool rx, bool tx)
 {
-	u32 value;
+	u32 value = 0;
 
 	regmap_read(priv->regs, MTK_STAR_REG_INT_MASK, &value);
 
