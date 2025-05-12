boundarydevices/linux

GENIO: net: stmmac: disable stop rxc when enable EEE
[Details]
DMA initialization and SW reset need rxc, if enable stop rxc
when in EEE, DMA initialization and SW reset will be fail.

Change-Id: If81b834632c41e63bc4052939fd0b13c9bbb82cc
Signed-off-by: fanyi zhang <fanyi.zhang@mediatek.com>

drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
@@ -468,6 +468,9 @@ stmmac_probe_config_dt(struct platform_device *pdev, u8 *mac)
 	plat->en_tx_lpi_clockgating =
 		of_property_read_bool(np, "snps,en-tx-lpi-clockgating");
 
+	plat->en_rx_lpi_clockgating =
+		of_property_read_bool(np, "snps,en-rx-lpi-clockgating");
+
 	/* Set the maxmtu to a default of JUMBO_LEN in case the
 	 * parameter is not present in the device tree.
 	 */

YANXIAOXIH/kernel

Signed-off-by: Jian Hui Lee <jianhui.lee@canonical.com>
Signed-off-by: Hsuan-Yu Lin <shane.lin@canonical.com>

drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
@@ -488,6 +488,9 @@ stmmac_probe_config_dt(struct platform_device *pdev, u8 *mac)
 	plat->en_tx_lpi_clockgating =
 		of_property_read_bool(np, "snps,en-tx-lpi-clockgating");
 
+	plat->rx_clk_runs_in_lpi=
+		of_property_read_bool(np, "snps,en-rx-lpi-clockgating");
+
 	/* Set the maxmtu to a default of JUMBO_LEN in case the
 	 * parameter is not present in the device tree.
 	 */